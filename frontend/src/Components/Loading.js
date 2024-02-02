a77c13ce9abd97957897d819f279513ee3d60b28
rohithr04 <rrohithramesh04@gmail.com>
2024-02-02 14:12:36 +0530

    main

--- frontend/src/Components/Loading.js
+++ frontend/src/Components/Loading.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Loading = () => {
   return (
-    <div class="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+    <div class="flex justify-center items-center h-screen">
+    <div class="relative w-24 h-24 animate-spin rounded-full bg-gradient-to-r from-purple-400 via-blue-500 to-red-400 ">
+        <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-20 h-20 bg-gray-200 rounded-full border-2 border-white"></div>
+    </div>
+</div>
   )
 }
 
